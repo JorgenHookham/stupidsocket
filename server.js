912f7d3553751d0f4aa45dde1a616001511d3d5c
Jorgen Hookham <Jorgen@Hookham.co>
2018-09-17 16:06:50 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = express()
+	.use(bodyParser.json())
+	.post('/broadcast', (req, res) => {
+		broadcast(req.body);
+		res.send('OK');
+	})
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+	console.log('Client connected');
+
+	ws.on('close', () => {
+		console.log('Client disconnected')
+	});
+
+	ws.on('message', (data) => {
+		ws.secret = data;
+	});
+});
+
+setInterval(() => {
+	broadcast({type: 'checkin', message: new Date().toTimeString()});
+}, 10000);
+
+function broadcast (data) {
+	console.log('Broadcast', data);
+	wss.clients.forEach(function each(client) {
+		client.send(JSON.stringify(data));
+	});
+}
