1971082ee737db1342a94fc716495ad48beb77d3
Jorgen Hookham <Jorgen@Hookham.co>
2018-09-17 16:45:23 -0700

    refactor

--- server.js
+++ server.js
@@ -9,14 +9,8 @@ const server = express()
 	.use(bodyParser.json())
 	.post('/broadcast', (req, res) => {
 		console.log('Accepted request');
-		broadcast(req.body);
+		console.log(req, req.body, req.header)
 		res.send('OK');
 		console.log('Request complete');
 	})
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-function broadcast (data) {
-	console.log('Beginning broadcast');
-	console.log('Broadcast', data);
-	console.log('Broadcast complete');
-}
