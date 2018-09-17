2847415a5fc83c31be4a655a867b4ccd174ea233
Jorgen Hookham <Jorgen@Hookham.co>
2018-09-17 16:22:26 -0700

    remove socket

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const express = require('express');
-const SocketServer = require('ws').Server;
 const bodyParser = require('body-parser');
-const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,27 +13,6 @@ const server = express()
 	})
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const wss = new SocketServer({ server });
-
-wss.on('connection', (ws) => {
-	console.log('Client connected');
-
-	ws.on('close', () => {
-		console.log('Client disconnected')
-	});
-
-	ws.on('message', (data) => {
-		ws.secret = data;
-	});
-});
-
-setInterval(() => {
-	broadcast({type: 'checkin', message: new Date().toTimeString()});
-}, 10000);
-
 function broadcast (data) {
 	console.log('Broadcast', data);
-	wss.clients.forEach(function each(client) {
-		client.send(JSON.stringify(data));
-	});
 }
