19497093b52e42af90db5c4744a6eafe359af968
Jorgen Hookham <Jorgen@Hookham.co>
2018-09-17 16:41:14 -0700

    add logging

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const PORT = process.env.PORT || 3000;
 const server = express()
 	.use(bodyParser.json())
 	.post('/broadcast', (req, res) => {
+		console.log('Accepted request');
 		broadcast(req.body);
 		res.send('OK');
-	})
-	.get('/broadcast', (req, res) => {
-		broadcast(req.body);
-		res.send('OK');
+		console.log('Request complete');
 	})
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 function broadcast (data) {
+	console.log('Beginning broadcast');
 	console.log('Broadcast', data);
+	console.log('Broadcast complete');
 }
